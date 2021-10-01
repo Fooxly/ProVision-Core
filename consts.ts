012a3209917cc5929670a237e41496e7052643da
Thom van den Broek <thombroek1606@hotmail.com>
2021-10-01 12:22:02 +0200

    updated teh fallback keywords

--- consts.ts
+++ consts.ts
@@ -1,28 +1,25 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export const FALLBACK_KEYWORDS = {
     TODO: {
+        group: "notes",
         color: "#fff",
         backgroundColor: "#f2b01f",
         overviewRulerColor: "rgba(242, 176, 31, 0.8)",
         overviewRulerLane: 4,
-        isWholeLine: false,
-        group: "notes",
     },
     FIXME: {
+        group: "notes",
         color: "#fff",
         backgroundColor: "#d85f88",
         overviewRulerColor: "rgba(216, 95, 136, 0.8)",
         overviewRulerLane: 4,
-        isWholeLine: false,
-        group: "notes",
     },
     NOTE: {
+        group: "notes",
         color: "#aaa",
         backgroundColor: "#434343",
         overviewRulerColor: "rgba(67, 67, 67, 0.8)",
         overviewRulerLane: 4,
-        isWholeLine: false,
-        group: "notes",
     },
 };
 
